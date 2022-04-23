eda4387e834d73184788957adaa3b1d5b48a7efb
woodman <80289981@qq.com>
2022-04-23 21:04:06 +0800

    添加VERION环境量等

--- homework/module1/httpserver.go
+++ homework/module1/httpserver.go
@@ -4,21 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	fmt.Println("http server starting ...")
+	os.Setenv("VERSION", "1.0.0")
 	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/localhost/healthz", healthzHandler)
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
 
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+}
 func rootHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("entering root handler")
 	requestHeader := r.Header
 	for k, vs := range requestHeader {
-		fmt.Printf("head key : %s ; header value: %s\n", k, vs)
 		for _, v := range vs {
 			w.Header().Add(k, v)
 		}
 	}
+	version := os.Getenv("VERSION")
+	w.Header().Add("VERSION", version)
+	log.Println(r.RemoteAddr)
 }
